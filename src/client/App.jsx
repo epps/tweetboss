c5a8c757c581cb64b66faa55cdaea26da8959eee
Erik Eppel <eceppel@gmail.com>
2018-07-13 21:40:37 -0700

    completes basic OAuth 1.0a signin flow w/o token persistence

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.signIn = this.signIn.bind(this);
+	}
+
+	signIn() {
+		axios.get('/api/sign-in').then(response => {
+			console.log('Response: ', response);
+			location.href = response.data.redirectUrl;
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Tweetboss</h1>
+				<button onClick={this.signIn}>Sign In</button>
 			</div>
 		);
 	}
--- src/server/index.js
+++ src/server/index.js
@@ -3,5 +3,77 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const qs = require('querystring');
+const url = require('url');
+const request = require('request');
+
 app.use(express.static('dist'));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+let oauthToken;
+let oauthTokenSecret;
+
+app.get('/api/sign-in', (req, res) => {
+	request.post(
+		{
+			url: 'https://api.twitter.com/oauth/request_token',
+			oauth: {
+				callback: 'http://localhost:8080/api/oauth/oauth-verifier',
+				consumer_key: process.env.CONSUMER_KEY,
+				consumer_secret: process.env.CONSUMER_SECRET
+			}
+		},
+		(error, response, body) => {
+			const responseData = qs.parse(body);
+
+			console.log('RESPONSE DATA ', responseData);
+
+			oauthToken = responseData.oauth_token;
+			oauthTokenSecret = responseData.oauth_token_secret;
+
+			const redirectUrl = `https://api.twitter.com/oauth/authenticate?oauth_token=${oauthToken}`;
+
+			res.send({ redirectUrl: redirectUrl });
+		}
+	);
+});
+
+app.get('/api/oauth/oauth-verifier', (req, res) => {
+	console.log(req.url);
+
+	const queryData = url.parse(req.url, true).query;
+
+	console.log(
+		`Do the two oauth_tokens match? ... ${queryData.oauth_token === oauthToken}`
+	);
+
+	request.post(
+		{
+			url: 'https://api.twitter.com/oauth/access_token',
+			oauth: {
+				consumer_key: process.env.CONSUMER_KEY,
+				consumer_secret: process.env.CONSUMER_SECRET,
+				token: queryData.oauth_token,
+				token_secret: oauthTokenSecret,
+				verifier: queryData.oauth_verifier
+			}
+		},
+		(error, response, body) => {
+			const responseData = qs.parse(body);
+
+			res.cookie(
+				'userDetails',
+				`userId=${responseData.user_id}&screenName=${responseData.screen_name}`,
+				{ maxAge: 900000 }
+			);
+
+			res.redirect('http://localhost:3000');
+		}
+	);
+
+	// res.redirect('/');
+	// res.redirect('http://localhost:3000');
+});
+
+app.listen(8080, () => {
+	console.log('Listening on port 8080!');
+});
